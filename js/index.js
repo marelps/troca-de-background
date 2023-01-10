330b27c345668f38acff7431fccb2028e8457b4c
Vitória Garrucho <vitoriagarrucho@gmail.com>
2023-01-10 16:42:44 -0300

    Adicionando arquivos antigos
    Esses arquivos são muito antigos, vou editar e melhorar o site

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function bg1() {
+    document.getElementById("bd").style.backgroundImage = "url('bg1.png')";
+}
+function bg2() {
+    document.getElementById("bd").style.backgroundImage = "url('bg2.png')";
+}
+function bg3() {
+    document.getElementById("bd").style.backgroundImage = "url('bg3.png')";
+}
+function retorno() {
+    document.getElementById("bd").style.background = "#E3D2F4";
+}
